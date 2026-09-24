4d6e03a808408c57bbdafae2362e0107ce7e4412
aeron-cap <aeroncaponpon.01@gmail.com>
2026-09-24 21:56:50 +0800

    fix: get intersts will have no filters

--- internal/database/store.go
+++ internal/database/store.go
@@ -73,7 +73,7 @@ type Interest struct {
 }
 
 func (s *Store) GetInterests(ctx context.Context) ([]Interest, error) {
-	const stmt = `SELECT id, keyword, weight, is_main, anchor, is_active FROM interests WHERE weight > 0`
+	const stmt = `SELECT id, keyword, weight, is_main, anchor, is_active FROM interests`
 
 	rows, err := s.db.QueryContext(ctx, stmt)
 	if err != nil {
